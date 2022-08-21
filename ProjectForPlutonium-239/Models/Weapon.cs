e53dde01068d82c1863d5644c486fa0cec1febe2
elnurra <elnurra@code.edu.az>
2022-08-22 00:24:44 +0400

    Task succesfully completed

---
+++ ProjectForPlutonium-239/Models/Weapon.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectForPlutonium_239.Models
+{
+    class Weapon
+    {
+        private int _magazineCapacity;
+        private int _ammoCount;
+        private double _bulletsUnloadTime;
+        private char _shootingMode;
+
+
+
+        public int MagazineCapacity
+        {
+            get
+            {
+                return _magazineCapacity;
+
+            }
+            set
+            {
+                if (MagazineCapacity >= 0)
+                {
+                    _magazineCapacity = value;
+                }
+            }
+        }
+
+        public int AmmoCount
+        {
+            get
+            {
+                return _ammoCount;
+
+            }
+            set
+            {
+                if (AmmoCount <= MagazineCapacity && AmmoCount >= 0)
+                {
+                    _ammoCount = value;
+
+                }
+            }
+        }
+
+        public double BulletsUnloadTime
+        {
+            get
+            {
+                return _bulletsUnloadTime;
+
+            }
+            set
+            {
+                if (BulletsUnloadTime >= 0)
+                {
+                    _bulletsUnloadTime = value;
+
+                }
+            }
+        }
+
+
+
+
+
+
+        public char ShootingMode
+        {
+            get
+            {
+                return _shootingMode;
+
+            }
+            set
+            {
+                if (value == 's' || value == 'S' || value == 'f' || value == 'F')
+                {
+                    _shootingMode = value;
+                }
+
+            }
+        }
+
+        public Weapon(int MagazineCapacity, int AmmoCount, double AmmoFullyTime, char ShootingMode)
+        {
+            this.MagazineCapacity = MagazineCapacity;
+            this.AmmoCount = AmmoCount;
+            this.BulletsUnloadTime = AmmoFullyTime;
+            this.ShootingMode = ShootingMode;
+        }
+
+
+
+        public void Shoot()
+        {
+
+
+
+            if (_ammoCount > 0)
+            {
+                _ammoCount--;
+
+                Console.WriteLine("BAM BAM");
+            }
+            else
+            {
+
+                Console.WriteLine("Need to reload your weapon: ");
+
+            }
+
+        }
+        public void Fire()
+        {
+
+            double time;
+            if (ShootingMode == 's' || ShootingMode == 'S')
+            {
+                Shoot();
+                time = (1 * BulletsUnloadTime / _magazineCapacity);
+
+                Console.WriteLine("Time for unload 1 bullet: " + time);
+
+            }
+            else if (ShootingMode == 'f' || ShootingMode == 'F')
+            {
+
+                time = ((_ammoCount * BulletsUnloadTime) / _magazineCapacity);
+                do
+                {
+                    if (AmmoCount != 0)
+                    {
+                        AmmoCount--;
+                        Console.WriteLine(AmmoCount);
+                        ;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Need to reload: ");
+                    }
+
+                } while (AmmoCount > 0);
+
+                Console.WriteLine("Time for unload all bullets: " + time + " seconds");
+            }
+
+        }
+
+        public int GetRemainBulletCount()
+        {
+            Console.WriteLine("Remain bullet count: ");
+            return (_magazineCapacity - _ammoCount);
+        }
+
+        public void Reload()
+        {
+            Console.WriteLine("Reloading... ");
+            AmmoCount = _magazineCapacity;
+            Console.WriteLine("Reloaded");
+        }
+
+        public void ChangeFireMode()
+        {
+            if (_shootingMode == 's' || _shootingMode == 'S')
+            {
+                _shootingMode = 'f';
+                _shootingMode = 'F';
+            }
+            else if (_shootingMode == 'f' || _shootingMode == 'F')
+            {
+                _shootingMode = 's';
+                _shootingMode = 'S';
+            }
+
+
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Current magazine capacity: {MagazineCapacity}" +
+                $"\nCurrent bullets count: {AmmoCount}" +
+                $"\nCurrent fully time to unload all bullets {BulletsUnloadTime} " +
+                $"\nShooting mode (s or S) - Single (f-F) - Fire,automatic: {ShootingMode} ");
+
+        }
+    }
+}
---
+++ ProjectForPlutonium-239/Program.cs
@@ -0,0 +1,135 @@
+﻿using ProjectForPlutonium_239.Models;
+using System;
+
+namespace ProjectForPlutonium_239
+{
+    
+            
+
+
+    
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+                try
+                {
+                    char input;
+                    Console.WriteLine("Input your magazine capacity: ");
+                    int magazinecapacity = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Input your ammo count:  ");
+                    int ammocount = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Write your time fully ");
+                    double bulletsUnloadTime = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Choose shooting mode: (s or S) Single shoot (f or F) Fire,automatic shoot");
+                    char shootingmode = char.Parse(Console.ReadLine());
+
+                    if (magazinecapacity >= ammocount)
+                    {
+                        Weapon weapon = new Weapon(magazinecapacity, ammocount, bulletsUnloadTime, shootingmode);
+
+                        Console.WriteLine($"Choose what u want to do: " +
+                            $"\n(1) Shoot: " +
+                            $"\n(2) Fire: " +
+                            $"\n(3) Get remain bullet count: " +
+                            $"\n(4) Reload: " +
+                            $"\n(5) Change fire mode: " +
+                            $"\n(6) Exit the program:" +
+                            $"\n(7) Change options:");
+                        do
+                        {
+
+                            input = char.Parse(Console.ReadLine());
+                            switch (input)
+                            {
+                                case '0':
+                                    weapon.GetInfo();
+                                    break;
+                                case '1':
+                                    weapon.Shoot();
+                                    break;
+                                case '2':
+                                    weapon.Fire();
+                                    break;
+                                case '3':
+                                    Console.WriteLine(weapon.GetRemainBulletCount());
+                                    break;
+                                case '4':
+                                    weapon.Reload();
+                                    break;
+                                case '5':
+                                    Console.WriteLine("Changing fire mode...");
+                                    weapon.ChangeFireMode();
+                                    Console.WriteLine("Changed fire mode.");
+                                    break;
+                                case '7':
+                                    Console.WriteLine($"What are u want change? " +
+                            $"\n (S) Change magazine capacity: " +
+                            $"\n (T) Change count of bullets: " +
+                            $"\n (D) Reset the time: ");
+                                    char change = char.Parse(Console.ReadLine());
+                                    switch (change)
+                                    {
+                                        case 'S':
+                                            magazinecapacity = int.Parse(Console.ReadLine());
+                                            weapon.MagazineCapacity = magazinecapacity;
+                                            Console.WriteLine("Changed magazine capacity.");
+
+                                            break;
+                                        case 'T':
+                                            ammocount = int.Parse(Console.ReadLine());
+                                            if (ammocount <= magazinecapacity)
+                                            {
+                                                weapon.AmmoCount = ammocount;
+                                                Console.WriteLine("Changed current ammo counts");
+                                            }
+                                            else
+                                            {
+                                                Console.WriteLine("Ammo count can't be more than magazine capacity: " + magazinecapacity);
+                                            }
+
+                                            break;
+                                        case 'D':
+                                            bulletsUnloadTime = int.Parse(Console.ReadLine());
+                                            weapon.BulletsUnloadTime = bulletsUnloadTime;
+                                            break;
+                                        default:
+                                            Console.WriteLine("Wrong input!");
+                                            break;
+                                    }
+                                    break;
+
+                                default:
+                                    Console.WriteLine("Wrong input!");
+                                    break;
+                            }
+
+
+                        } while (input != '6');
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ammo count can't be more than magazine Capacity! ");
+                    }
+                }
+                catch (Exception exept)
+                {
+
+                    Console.WriteLine(exept.Message);
+                }
+
+
+
+
+
+
+
+            }
+        }
+    }
+
+
+   
+
